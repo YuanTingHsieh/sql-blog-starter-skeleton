f5d6931e447119269c679728c8ac6c2e699f0cee
YuanTingHsieh <b01901046@ntu.edu.tw>
2016-12-20 16:04:50 +0000

    gggg

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,22 +1,32 @@
 import { Router } from 'express';
+import bcrypt from 'bcryptjs';
+import thenify from 'thenify';
 
 import models from '../models';
 
 const { User } = models;
+
+const genSalt = thenify(bcrypt.genSalt);
+const hash = thenify(bcrypt.hash);
 
 const articleRouter = new Router();
 
 articleRouter.post('/signup', async (req, res) => {
   const { email, password, name } = req.body;
 
-  const user = await User.create({
-    email, password, name,
-  });
-
-
-  res.json({
+  genSalt(10)
+  .then(res => hash(password, res))
+  .then(newpass => {
+    const user = User.create({
+      email, newpass, name,
+    });
+    resolve(user)
+  })
+  .then(user => res.json({
     createdId: user.id,
-  });
+  }))
+
+
 });
 
 export default articleRouter;
